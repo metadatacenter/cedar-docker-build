ca00337e804a716e4f6dd20d406e949061c1979a
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-05-26 10:51:50 -0700

    Keycloak with mysql

--- cedar-keycloak/scripts/docker-entrypoint.sh
+++ cedar-keycloak/scripts/docker-entrypoint.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-export POSTGRES_PORT_5432_TCP_ADDR="${CEDAR_POSTGRES_HOST}"
-export POSTGRES_PORT_5432_TCP_PORT="${CEDAR_POSTGRES_PORT}"
-export POSTGRES_PASSWORD="${CEDAR_POSTGRES_PASSWORD}"
-export POSTGRES_USER="${CEDAR_POSTGRES_USER}"
-
 sed -i 's/<cedar.CEDAR_HOST>/'${CEDAR_HOST}'/g' /opt/jboss/realm-import/cedar.realm.json
 
 if [ $CEDAR_KEYCLOAK_ADMIN_USER ] && [ $CEDAR_KEYCLOAK_ADMIN_PASSWORD ]; then
     keycloak/bin/add-user-keycloak.sh --user $CEDAR_KEYCLOAK_ADMIN_USER --password $CEDAR_KEYCLOAK_ADMIN_PASSWORD
 fi
 
-echo "Waiting for Postgres to be available on port ${CEDAR_POSTGRES_PORT}"
-while ! ncat ${CEDAR_POSTGRES_HOST} ${CEDAR_POSTGRES_PORT} </dev/null >/dev/null; do
-  echo "Postgres not available yet, waiting ..."
+echo "Waiting for MySQL to be available at ${CEDAR_MYSQL_HOST}:${CEDAR_MYSQL_PORT}}"
+while ! ncat ${CEDAR_MYSQL_HOST} ${CEDAR_MYSQL_PORT} </dev/null >/dev/null; do
+  echo "MySQL not available yet, waiting ..."
   sleep 1
 done
-echo "Postgres is up, launching Keycloak"
+echo "MySQL is up, launching Keycloak"
+
+env
 
 exec /opt/jboss/keycloak/bin/standalone.sh $@
 exit $?
