b89cfdda8759e9d557443ec05e632b2028d01917
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-01 00:04:45 -0700

    Dockerized repo and resource server

--- cedar-microservice/scripts/docker-entrypoint.sh
+++ cedar-microservice/scripts/docker-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Import CA cert"
+echo "yes" | $JAVA_HOME/bin/keytool -import -trustcacerts -file ${CEDAR_HOME}/ca/ca-cedar.crt -alias cedar -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit
+
 echo --------------------------------------------------------------------------------
 echo Starting CEDAR ${CEDAR_SERVER_NAME} server
 echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
