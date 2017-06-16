6321e35347982639124cf22649ad9cb774e8ffad
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-16 11:32:15 -0700

    Changed waiting message

--- cedar-admin-tool/scripts/docker-entrypoint.sh
+++ cedar-admin-tool/scripts/docker-entrypoint.sh
@@ -3,7 +3,7 @@ echo "Import CA cert"
 echo "yes" | $JAVA_HOME/bin/keytool -import -trustcacerts -file ${CEDAR_HOME}/ca/ca-cedar.crt -alias cedar -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit
 echo --------------------------------------------------------------------------------
 echo
-echo "Waiting for the miracle"
+echo "Waiting for users"
 while true
 do
   sleep 10
