565c9896bf4014c0bb46c45f7d405196a98a4626
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-29 15:02:10 -0700

    Admin tool with jar from nexus server, and README

--- cedar-admin-tool/include/install_deps.sh
+++ cedar-admin-tool/include/install_deps.sh
@@ -4,7 +4,7 @@ echo "Current working directory:"
 pwd
 
 echo "Downloading microservice jar:"
-JAR_URL=${CEDAR_MICROSERVICE_JAR_BASE}/cedar-admin-tool-${CEDAR_VERSION}.jar
+JAR_URL=${CEDAR_MICROSERVICE_JAR_BASE}/org/metadatacenter/cedar-admin-tool/${CEDAR_VERSION}/cedar-admin-tool-${CEDAR_VERSION}.jar
 echo ${JAR_URL}
 curl -o cedar-admin-tool.jar ${JAR_URL}
 echo "Contents:"
