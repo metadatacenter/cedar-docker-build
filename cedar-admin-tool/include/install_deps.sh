10459903e4c9ca278626881c2c8e8317470bf01f
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-08-18 16:22:46 -0700

    Do not rename jar

--- cedar-admin-tool/include/install_deps.sh
+++ cedar-admin-tool/include/install_deps.sh
@@ -7,7 +7,5 @@ ARTIFACT=cedar-admin-tool
 
 echo "Downloading admin tool jar:"
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy -Dartifact=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION}:jar -DoutputDirectory=. -Dmdep.useBaseVersion=true -Dmdep.stripVersion=true
-echo "Renaming admin tool jar:"
-mv ./${ARTIFACT}.jar ./cedar-admin-tool.jar
 echo "Contents of current directory:"
 ls -ls
