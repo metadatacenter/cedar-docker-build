b77cde283b452843074632e2c97c5b5e81657b4c
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-05-31 14:52:18 -0700

    Fix mysql detectionfor messaging server

--- cedar-microservice/scripts/wait-and-init-mysql.py
+++ cedar-microservice/scripts/wait-and-init-mysql.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
+import sys
+import time
 
 import MySQLdb
-import time
-from MySQLdb import OperationalError
+from MySQLdb import MySQLError
 
 
 def execute_db_code(title_message, code_to_execute, error_message):
@@ -13,12 +14,12 @@ def execute_db_code(title_message, code_to_execute, error_message):
         connection = MySQLdb.connect(host=mysql_host, port=mysql_port, user=mysql_root_user, passwd=mysql_root_password)
         if code_to_execute is not None:
             code_to_execute(connection)
-    except OperationalError as e:
-        print error_message
-        print e
+    except MySQLError as e:
+        print(error_message)
+        print(e)
         return False
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print("Unexpected error:" + sys.exc_info()[0])
         return False
     finally:
         if connection is not None:
@@ -31,7 +32,7 @@ def connect_with_root():
 
 
 def wait_for_root():
-    print "Wait for root user connection ..."
+    print("Wait for root user connection ...")
     number_of_successes = 0
     number_of_failures = 0
     while number_of_successes < number_of_tries:
@@ -41,7 +42,7 @@ def wait_for_root():
             number_of_failures += 1
             number_of_successes = 0
         time.sleep(sleep_seconds)
-        print "\tFailures:{},\tSuccesses:{}".format(number_of_failures, number_of_successes)
+        print("\tFailures:{},\tSuccesses:{}".format(number_of_failures, number_of_successes))
 
 
 def create_database_core(connection):
@@ -49,7 +50,7 @@ def create_database_core(connection):
 
     create_database_query = "CREATE DATABASE IF NOT EXISTS `{database}`"
     q1 = create_database_query.format(database=application_database)
-    print q1
+    print(q1)
     cursor.execute(q1)
 
 
@@ -60,7 +61,7 @@ def create_database():
 def create_application_user_core(connection):
     cursor = connection.cursor()
 
-    print "Dropping application user if exists ..."
+    print("Dropping application user if exists ...")
     drop_user_query = "DROP USER IF EXISTS '{user}'@'{host}'"
     q1 = drop_user_query.format(user=application_user, host=localhost)
     q2 = drop_user_query.format(user=application_user, host=mysql_host)
@@ -69,7 +70,7 @@ def create_application_user_core(connection):
     cursor.execute(q1)
     cursor.execute(q2)
 
-    print "Creating application user ..."
+    print("Creating application user ...")
     create_user_query = "CREATE USER '{user}'@'{host}' IDENTIFIED BY '{passwd}'"
     q1 = create_user_query.format(user=application_user, host=localhost, passwd=application_password)
     q2 = create_user_query.format(user=application_user, host=mysql_host, passwd=application_password)
@@ -78,7 +79,7 @@ def create_application_user_core(connection):
     cursor.execute(q1)
     cursor.execute(q2)
 
-    print "Granting all privileges to application user ..."
+    print("Granting all privileges to application user ...")
     grant_query = "GRANT ALL PRIVILEGES ON {database}.* TO '{user}'@'{host}'"
     q1 = grant_query.format(user=application_user, host=localhost, database=application_database)
     q2 = grant_query.format(user=application_user, host=mysql_host, database=application_database)
@@ -93,7 +94,7 @@ def create_application_user():
                            "Error while creating application user")
 
 
-print "Reading environment variables"
+print("Reading environment variables")
 mysql_host = os.environ.get('CEDAR_MESSAGING_MYSQL_HOST')
 mysql_port = int(os.environ.get('CEDAR_MESSAGING_MYSQL_PORT'))
 mysql_root_user = "root"
@@ -106,16 +107,16 @@ localhost = '127.0.0.1'
 number_of_tries = 5
 sleep_seconds = 1
 
-print "---- Server info ----"
-print "MySQL server host   :" + mysql_host
-print "MySQL server port   :" + str(mysql_port)
-print "Root user           :" + mysql_root_user
+print("---- Server info ----")
+print("MySQL server host   :" + mysql_host)
+print("MySQL server port   :" + str(mysql_port))
+print("Root user           :" + mysql_root_user)
 # print "Root password       :" + mysql_root_password
-print "Application user    :" + application_user
+print("Application user    :" + application_user)
 # print "Application password:" + application_password
-print "Application database:" + application_database
+print("Application database:" + application_database)
 
-print "Wait for MySQL server to be available"
+print("Wait for MySQL server to be available")
 
 wait_for_root()
 create_database()
