86a5cbe487d7558e990e4f261d5b31db43d19b84
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-06-04 17:26:51 -0700

    Nginx roots

--- cedar-nginx-setup/scripts/docker-entrypoint.sh
+++ cedar-nginx-setup/scripts/docker-entrypoint.sh
@@ -6,37 +6,21 @@ echo "Using CEDAR_HOST              :${CEDAR_HOST}"
 
 sed -i 's/<cedar.CEDAR_HOST>/'${CEDAR_HOST}'/g' /etc/nginx/conf.d/default.conf
 
-mkdir -p /usr/share/nginx/1
-touch /usr/share/nginx/1/index.html
-mkdir -p /usr/share/nginx/2
-touch /usr/share/nginx/2/index.html
-mkdir -p /usr/share/nginx/3
-touch /usr/share/nginx/3/index.html
-mkdir -p /usr/share/nginx/4
-touch /usr/share/nginx/4/index.html
-mkdir -p /usr/share/nginx/5
-touch /usr/share/nginx/5/index.html
-mkdir -p /usr/share/nginx/6
-touch /usr/share/nginx/6/index.html
-mkdir -p /usr/share/nginx/7
-touch /usr/share/nginx/7/index.html
-mkdir -p /usr/share/nginx/8
-touch /usr/share/nginx/8/index.html
-mkdir -p /usr/share/nginx/9
-touch /usr/share/nginx/9/index.html
-mkdir -p /usr/share/nginx/10
-touch /usr/share/nginx/10/index.html
-mkdir -p /usr/share/nginx/11
-touch /usr/share/nginx/11/index.html
-mkdir -p /usr/share/nginx/12
-touch /usr/share/nginx/12/index.html
-mkdir -p /usr/share/nginx/13
-touch /usr/share/nginx/13/index.html
-mkdir -p /usr/share/nginx/14
-touch /usr/share/nginx/14/index.html
-mkdir -p /usr/share/nginx/15
-touch /usr/share/nginx/15/index.html
-mkdir -p /usr/share/nginx/16
-touch /usr/share/nginx/16/index.html
+mkdir -p /usr/share/nginx/_
+mkdir -p /usr/share/nginx/auth
+mkdir -p /usr/share/nginx/cedar
+mkdir -p /usr/share/nginx/group
+mkdir -p /usr/share/nginx/messaging
+mkdir -p /usr/share/nginx/repo
+mkdir -p /usr/share/nginx/resource
+mkdir -p /usr/share/nginx/schema
+mkdir -p /usr/share/nginx/submission
+mkdir -p /usr/share/nginx/template
+mkdir -p /usr/share/nginx/terminology
+mkdir -p /usr/share/nginx/user
+mkdir -p /usr/share/nginx/valuerecommender
+mkdir -p /usr/share/nginx/worker
+mkdir -p /usr/share/nginx/workspace
+mkdir -p /usr/share/nginx/www
 
 exec "$@"
