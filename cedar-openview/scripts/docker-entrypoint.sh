3f484fd44190ea2b0dd2c1cd32a94c04086d5e63
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-24 20:56:58 -0700

    open frontend runtime config

--- cedar-openview/scripts/docker-entrypoint.sh
+++ cedar-openview/scripts/docker-entrypoint.sh
@@ -5,4 +5,6 @@ echo "Executing sed"
 
 sed -i 's/\.metadatacenter\.org\//\.'${CEDAR_HOST}'\//g' ${CEDAR_OPENVIEW_HOME}/index.html
 
+sed -i 's/\.metadatacenter\.org\//\.'${CEDAR_HOST}'\//g' ${CEDAR_OPENVIEW_HOME}/assets/data/appConfig.json
+
 exec "$@"
