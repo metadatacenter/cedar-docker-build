c8a232144e9ac42308edc2424157c8e7949ee7b8
Attila L. Egyedi <egyedia@stanford.edu>
2023-05-19 18:47:22 -0700

    Add inner and outer retry to  dependency plugin

--- cedar-microservice/include/install_deps.sh
+++ cedar-microservice/include/install_deps.sh
@@ -4,10 +4,19 @@ echo "Current working directory:"
 pwd
 
 ARTIFACT=cedar-${CEDAR_SERVER_NAME}-server-application
+REPO_URL=https://nexus.bmir.stanford.edu/
+ARTIFACT_FULL=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION}
 
 echo "Downloading microservice jar:"
-mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -DrepoUrl=https://nexus.bmir.stanford.edu/ -Dartifact=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION}
-mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy -Dartifact=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION}:jar -DoutputDirectory=. -Dmdep.useBaseVersion=true -Dmdep.stripVersion=true
+n=0
+until [ "$n" -ge 5 ]
+do
+  mvn org.apache.maven.plugins:maven-dependency-plugin:3.5.0:get -DrepoUrl=${REPO_URL} -Dartifact=${ARTIFACT_FULL} -Dmaven.wagon.http.retryHandler.count=5 && break
+  echo "Downloading of jar failed. Retrying soon..."
+  n=$((n+1))
+  sleep $((n+15))
+done
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.5.0:copy -Dartifact=${ARTIFACT_FULL}:jar -DoutputDirectory=. -Dmdep.useBaseVersion=true -Dmdep.stripVersion=true
 echo "Renaming microservice jar:"
 mv ./${ARTIFACT}.jar ./cedar-server.jar
 echo "Contents of current directory:"
--- cedar-server-resource/include/install_cedarat_deps.sh
+++ cedar-server-resource/include/install_cedarat_deps.sh
@@ -6,7 +6,14 @@ pwd
 ARTIFACT=cedar-admin-tool
 
 echo "Downloading admin tool jar:"
-mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:get -DrepoUrl=https://nexus.bmir.stanford.edu/ -Dartifact=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION}
-mvn org.apache.maven.plugins:maven-dependency-plugin:3.1.1:copy -Dartifact=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION}:jar -DoutputDirectory=. -Dmdep.useBaseVersion=true -Dmdep.stripVersion=true
+n=0
+until [ "$n" -ge 5 ]
+do
+  mvn org.apache.maven.plugins:maven-dependency-plugin:3.5.0:get -DrepoUrl=https://nexus.bmir.stanford.edu/ -Dartifact=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION} -Dmaven.wagon.http.retryHandler.count=5 && break
+  echo "Downloading of jar failed. Retrying soon..."
+  n=$((n+1))
+  sleep $((n+15))
+done
+mvn org.apache.maven.plugins:maven-dependency-plugin:3.5.0:copy -Dartifact=org.metadatacenter:${ARTIFACT}:${CEDAR_VERSION}:jar -DoutputDirectory=. -Dmdep.useBaseVersion=true -Dmdep.stripVersion=true
 echo "Contents of current directory:"
 ls -ls
