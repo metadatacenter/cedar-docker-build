1e9e90277d55192a80ab7ab1c6e6daadbece9b48
Attila L. Egyedi <egyedia@stanford.edu>
2023-05-23 15:17:32 -0700

    Fix openview main.js

--- cedar-frontend-openview/scripts/docker-entrypoint.sh
+++ cedar-frontend-openview/scripts/docker-entrypoint.sh
@@ -5,6 +5,8 @@ echo "Executing sed"
 
 sed -i 's/\.metadatacenter\.org\//\.'${CEDAR_HOST}'\//g' ${CEDAR_FRONTEND_HOME}/index.html
 
+sed -i 's/\.metadatacenter\.org\//\.'${CEDAR_HOST}'\//g' ${CEDAR_FRONTEND_HOME}/main.js
+
 sed -i 's/\.metadatacenter\.org\//\.'${CEDAR_HOST}'\//g' ${CEDAR_FRONTEND_HOME}/assets/data/appConfig.json
 
 exec "$@"
